879326e50ce3a4bf337f711b9b59e6e01d7b1aa2
Serhii Korneliuk <seiger@meta.ua>
2023-12-26 23:37:35 +0200

    [FIX] Main menu.

--- plugins/sCommercePlugin.php
+++ plugins/sCommercePlugin.php
@@ -5,23 +5,26 @@
 
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Event;
+use Seiger\sCommerce\Facades\sCommerce;
 
 /**
  * Add Menu item
  */
 Event::listen('evolution.OnManagerMenuPrerender', function($params) {
-    $menu['scommerce'] = [
-        'scommerce',
-        'main',
-        '<i class="' . __('sCommerce::global.icon') . '"></i><span class="menu-item-text">' . __('sCommerce::global.title') . '</span>',
-        sCommerce::route('sCommerce.index'),
-        __('sCommerce::global.title'),
-        "",
-        "",
-        "main",
-        0,
-        8,
-    ];
+    if (evo()->getConfig('scom_in_main_menu', 0) == 1) {
+        $menu['scommerce'] = [
+            'scommerce',
+            'main',
+            '<i class="' . __('sCommerce::global.icon') . '"></i><span class="menu-item-text">' . __('sCommerce::global.title') . '</span>',
+            sCommerce::moduleUrl(),
+            __('sCommerce::global.title'),
+            "",
+            "",
+            "main",
+            0,
+            evo()->getConfig('scom_main_menu_order', 11),
+        ];
 
-    return serialize(array_merge($params['menu'], $menu));
+        return serialize(array_merge($params['menu'], $menu));
+    }
 });
--- src/sCommerce.php
+++ src/sCommerce.php
@@ -5,5 +5,13 @@ use Illuminate\Support\Str;
 
 class sCommerce
 {
-    //
+    /**
+     * Module url
+     *
+     * @return string
+     */
+    public function moduleUrl(): string
+    {
+        return 'index.php?a=112&id=' . md5(__('sCommerce::global.title'));
+    }
 }
