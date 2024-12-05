b6eb246a190d3eb960de6af196d7aa36a5018826
Serhii Korneliuk <seiger@meta.ua>
2024-12-05 16:07:32 +0200

    [ADD] Category Active Attribute.

---
+++ docs/pages/settings.md
@@ -0,0 +1,10 @@
+---
+layout: page
+title: Settings
+description: sCommerce Settings
+permalink: /settings/
+---
+
+## Catalog root
+
+`sCommerce::config('basic.catalog_root')` 
--- src/Models/sCategory.php
+++ src/Models/sCategory.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Facades\DB;
  * This class extends the SiteContent class and represents a category.
  *
  * @package Seiger\sCommerce
+ *
+ * @property-read int $active The Active state of the Category.
  */
 class sCategory extends SiteContent
 {
@@ -24,5 +26,15 @@ class sCategory extends SiteContent
     public function products()
     {
         return $this->belongsToMany(sProduct::class, 's_product_category', 'category', 'product')->withPivot('position');
+    }
+
+    /**
+     * Gets the active this category resource.
+     *
+     * @return int 1 if published and not deleted else 0.
+     */
+    public function getActiveAttribute(): int
+    {
+        return intval($this->published == 1 && $this->deleted == 0);
     }
 }
