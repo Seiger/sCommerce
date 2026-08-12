4e8eba145a4d43886143d892cc1ac5d8b11aabc5
Serhii Korneliuk <info@seigerit.com>
2026-08-12 03:28:58 +0300

    fix(manager): include CSRF token in module URLs

--- src/sCommerce.php
+++ src/sCommerce.php
@@ -536,7 +536,13 @@ class sCommerce
      */
     public function moduleUrl(): string
     {
-        return 'index.php?a=112&id=' . md5(__('sCommerce::global.title'));
+        $url = 'index.php?a=112';
+
+        if (!empty($_SESSION['mgrValidated'])) {
+            $url .= '&_token=' . rawurlencode(csrf_token());
+        }
+
+        return $url . '&id=' . md5(__('sCommerce::global.title'));
     }
 
     /**
